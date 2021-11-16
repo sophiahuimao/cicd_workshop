d15f6f9934c5210f7b846a345e9400e12d71e8a3
Lara Newsom <lnewsom@sourceallies.com>
2021-11-16 10:24:18 -0600

    altered user service to use Cognito Auth

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,13 +1,41 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import Auth from '@aws-amplify/auth';
 import { User } from './models/user';
 import { UserVotes } from './models/user-votes';
+import { Hub } from 'aws-amplify';
+
+export interface CognitoAuthUser {
+  attributes: unknown,
+  id: string,
+  username: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   currentUser: BehaviorSubject<User | undefined> = new BehaviorSubject<User | undefined>(undefined);
+
+  constructor(
+    private applicationRef: ApplicationRef
+  ) {
+    Auth.currentAuthenticatedUser().then(
+      (user: CognitoAuthUser) => this.setCurrentUser(user.username),
+      _err => this.currentUser.next(undefined)
+    );
+
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      const { username } = data;
+      if (event === 'signIn') {
+        this.currentUser.next(new User(username));
+        // manually trigger change detection on sign in
+        this.applicationRef.tick();
+      } else {
+        this.currentUser.next(undefined);
+      }
+    });
+  }
 
   getCurrentUser(): Observable<User | undefined>{
     return this.currentUser.asObservable();
